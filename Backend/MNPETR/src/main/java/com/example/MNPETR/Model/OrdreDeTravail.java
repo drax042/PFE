8fed3fa881761f0f33208484e51b9525559ce585
ghiles <ghiloux03@gmail.com>
2024-04-28 11:25:54 +0100

    Test

--- Backend/MNPETR/src/main/java/com/example/MNPETR/Model/OrdreDeTravail.java
+++ Backend/MNPETR/src/main/java/com/example/MNPETR/Model/OrdreDeTravail.java
@@ -1,10 +1,13 @@
 package com.example.MNPETR.Model;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.ToString;
 
 @Builder
 @ToString
+
+
 public class OrdreDeTravail {
     private int ID_OT ;
     private int Tot_Horaire ;
@@ -12,5 +15,5 @@ public class OrdreDeTravail {
     private String Description_Panne;
     private String Type_Otz ;
     private int Date ;
-    
+
 }
