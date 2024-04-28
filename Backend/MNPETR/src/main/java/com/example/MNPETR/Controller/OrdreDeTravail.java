019ba77a75bf50d11b55a31dd8cdd866b3149f00
ghiles <ghiloux03@gmail.com>
2024-04-28 11:07:12 +0100

    First Front Commit

--- Backend/MNPETR/src/main/java/com/example/MNPETR/Controller/OrdreDeTravail.java
+++
@@ -1,6 +0,0 @@
-package com.example.MNPETR.Controller;
-
-@
-public class OrdreDeTravail {
-
-}
---
+++ Backend/MNPETR/src/main/java/com/example/MNPETR/Model/OrdreDeTravail.java
@@ -0,0 +1,15 @@
+package com.example.MNPETR.Model;
+
+import lombok.Builder;
+import lombok.ToString;
+
+@Builder
+@ToString
+public class OrdreDeTravail {
+    private int ID_OT ;
+    private int Tot_Horaire ;
+    private int Duree ;
+    private String Description_Panne;
+    private String Type_Otz ;
+    private int Date ;
+}
