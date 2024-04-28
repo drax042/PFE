658a5d6b3828bb4edbc85c72fe537a00a4afab18
Adel-Aopatos <adelrebaine@gmail.com>
2024-04-28 09:58:57 +0100

    first commit - Adel

---
+++ Backend/MNPETR/src/main/java/com/example/MNPETR/MnpetrApplication.java
@@ -0,0 +1,13 @@
+package com.example.MNPETR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnpetrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnpetrApplication.class, args);
+	}
+
+}
