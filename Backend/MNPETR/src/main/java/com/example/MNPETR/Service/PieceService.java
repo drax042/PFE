69f40bdfd9b54f0b6b6d5e2b30a7010add219279
ghiles <ghiloux03@gmail.com>
2024-05-03 12:41:05 +0100

    Ajout du premier Repository PieceRepo.java et du premier service PieceService.java

---
+++ Backend/MNPETR/src/main/java/com/example/MNPETR/Service/PieceService.java
@@ -0,0 +1,39 @@
+package com.example.MNPETR.Service;
+
+import com.example.MNPETR.Model.Piece;
+import com.example.MNPETR.Repository.PieceRepo;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PieceService {
+    private final PieceRepo pieceRepo ;
+@Autowired
+    public PieceService(PieceRepo pieceRepo) {
+        this.pieceRepo = pieceRepo;
+    }
+
+    public List<Piece> getAllPiece(){
+        return pieceRepo.findAll() ;
+    }
+
+    public Optional<Piece> getPieceById(Integer ID_Piece ) {
+     return pieceRepo.findById(ID_Piece) ;
+
+    }
+
+    public  Piece savePiece (Piece piece){
+        return pieceRepo.save(piece) ;
+
+    }
+
+    public void deletePiece(Piece piece) {
+        pieceRepo.delete(piece);
+    }
+
+}
--- Frontend/mnpetr/src/App.js
+++ Frontend/mnpetr/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-      <img src="Sonatrach.svg.png" alt="Sonatrach.svg.png"/>
+      <img src='images/LogoSonatrach.png' alt="Sonatrach.svg.png"/>
   );
 }
 
